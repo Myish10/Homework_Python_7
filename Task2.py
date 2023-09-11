c53f9286737de765b22a2d9aee70a661764d5221
adya <myish1084@gmail.com>
2023-09-11 10:25:00 +0300

    Task 2.1

--- Task2.py
+++ Task2.py
@@ -16,7 +16,7 @@
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_columns + 1):
         for j in range(1, num_rows + 1):
-            print(f'\t{i * j}', end='')
+            print(f'\t{operation(i, j)}', end='')
         print()
 
 
